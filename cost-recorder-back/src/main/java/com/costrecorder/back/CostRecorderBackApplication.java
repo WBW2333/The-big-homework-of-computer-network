cf876031f7e8e4f90c3eb656ed3ecab57a1cd0a1
yinzihang <201250173@smail.nju.edu.cn>
2022-05-17 10:26:11 +0800

    init

---
+++ cost-recorder-back/src/main/java/com/costrecorder/back/CostRecorderBackApplication.java
@@ -0,0 +1,16 @@
+package com.costrecorder.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yinzihang
+ */
+@SpringBootApplication
+public class CostRecorderBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CostRecorderBackApplication.class, args);
+    }
+
+}
---
+++ cost-recorder-back/src/test/java/com/costrecorder/back/CostRecorderBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.costrecorder.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CostRecorderBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
